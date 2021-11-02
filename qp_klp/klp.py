131d600192ea8adf91036dbfc57975ff496ae250
Charles Cowart <ccowart@ucsd.edu>
2021-11-02 16:21:16 -0700

    Removing PipelineError catch-all

--- qp_klp/klp.py
+++ qp_klp/klp.py
@@ -18,7 +18,7 @@ from sequence_processing_pipeline.SequenceDirectory import SequenceDirectory
 from sequence_processing_pipeline.PipelineError import PipelineError
 from metapool import KLSampleSheet, validate_and_scrub_sample_sheet
 from subprocess import Popen, PIPE
-from os import environ
+from os import environ, walk
 from inspect import stack
 
 
@@ -53,24 +53,19 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
     skip_exec = True if [x for x in stack() if
                          'unittest' in x.filename] else False
 
-    try:
-        qclient.update_job_step(job_id, "Step 1 of 6: Setting up pipeline")
+    success = True
+    ainfo = None
+    msg = None
 
-        # Although KLSampleSheet thoroughly validates the sample-sheeet,
-        # verify that we at least have a body to write to file, etc.
-        if {'body', 'content_type', 'filename'} != set(sample_sheet):
-            raise PipelineError("This doesn't appear to be a valid sample "
-                                "sheet; please review.")
+    qclient.update_job_step(job_id, "Step 1 of 6: Setting up pipeline")
 
-        # the final_results directory is the subdirectory of out_dir where
-        # all final products are moved to. This is distinct from out_dir,
-        # which is used as a working directory.
+    if {'body', 'content_type', 'filename'} == set(sample_sheet):
         outpath = partial(join, out_dir)
         final_results_path = outpath('final_results')
         makedirs(final_results_path, exist_ok=True)
 
-        # the user is uploading a sample-sheet to us, but we need the sample-
-        # sheet as a file to pass to the Pipeline().
+        # the user is uploading a sample-sheet to us, but we need the
+        # sample-sheet as a file to pass to the Pipeline().
         sample_sheet_path = outpath(sample_sheet['filename'])
         with open(sample_sheet_path, 'w') as f:
             f.write(sample_sheet['body'])
@@ -81,19 +76,19 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
         if not val_sheet:
             qclient.update_job_step(job_id,
                                     "Sample sheet failed validation.")
-            raise PipelineError("Sample sheet failed validiation")
+            raise ValueError("Sample sheet failed validiation")
         else:
             # get project names and their associated qiita ids
             bioinformatics = val_sheet.Bioinformatics
             lst = bioinformatics.to_dict('records')
 
-        # find the uploads directory all trimmed files will need to be moved
-        # to.
+        # find the uploads directory all trimmed files will need to be
+        # moved to.
         results = qclient.get("/qiita_db/artifacts/types/")
 
-        # trimmed files are stored by qiita_id. Find the qiita_id associated
-        # with each project and ensure a subdirectory exists for when it comes
-        # time to move the trimmed files.
+        # trimmed files are stored by qiita_id. Find the qiita_id
+        # associated with each project and ensure a subdirectory exists
+        # for when it comes time to move the trimmed files.
         special_map = []
         for result in lst:
             project_name = result['Sample_Project']
@@ -102,16 +97,21 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
             makedirs(upload_path, exist_ok=True)
             special_map.append((project_name, upload_path))
 
-        # Create a Pipeline() object. Pipeline() will raise PipelineErrors()
-        # if files and directories don't exist as they should, etc. These are
-        # all caught below. SequenceDirectory() and Jobs() also raise
-        # PipelineErrors().
-        # Note that the location of the BCL files (input data) is discovered
-        # by Pipeline(), based on the run_identifier given, and the search
-        # paths stored in the file pointed to by config_fp. The input path
-        # to the BCL files is not passed directly to Pipeline by the user.
+        config_fp = ('/home/qiita_test/qiita-spots/'
+                     'qp-knight-lab-processing/qp_klp/configuration.json')
 
-        pipeline = Pipeline(CONFIG_FP, run_identifier, out_dir, job_id)
+        try:
+            pipeline = Pipeline(config_fp, run_identifier, out_dir, job_id)
+        except PipelineError as e:
+            # Pipeline is the object that finds the input fp, based on
+            # a search directory set in configuration.json and a run_id.
+            if str(e).endswith("could not be found"):
+                msg = f"A path for {run_identifier} could not be found."
+                return False, None, msg
+            else:
+                raise e
+
+        makedirs(join(pipeline.run_dir, run_identifier), exist_ok=True)
 
         sdo = SequenceDirectory(pipeline.run_dir, sample_sheet_path)
 
@@ -139,8 +139,8 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
             convert_job.run()
 
         qclient.update_job_step(job_id,
-                                "Step 3 of 6: Adaptor & Host [optional]"
-                                " trimming")
+                                "Step 3 of 6: Adaptor & Host [optional] "
+                                "trimming")
 
         raw_fastq_files_path = join(pipeline.output_path, 'ConvertJob')
 
@@ -164,8 +164,8 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
         if not skip_exec:
             qc_job.run()
 
-        qclient.update_job_step(job_id, "Step 4 of 6: Generating FastQC &"
-                                        " MultiQC reports")
+        qclient.update_job_step(job_id, "Step 4 of 6: Generating FastQC & "
+                                        "MultiQC reports")
 
         config = pipeline.configuration['fastqc']
 
@@ -193,8 +193,8 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
 
         project_list = fastqc_job.project_names
 
-        qclient.update_job_step(job_id, "Step 5 of 6: Generating Prep Info"
-                                        "rmation Files")
+        qclient.update_job_step(job_id, "Step 5 of 6: Generating Prep "
+                                        "Information Files")
 
         config = pipeline.configuration['seqpro']
         gpf_job = GenPrepFileJob(
@@ -214,8 +214,7 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
         qclient.update_job_step(job_id, "Step 6 of 6: Copying results to "
                                         "archive")
 
-        cmds = [f'cd {out_dir}; tar zcvf logs-ConvertJob.tgz ConvertJob/'
-                'logs',
+        cmds = [f'cd {out_dir}; tar zcvf logs-ConvertJob.tgz ConvertJob/logs',
                 f'cd {out_dir}; tar zcvf reports-ConvertJob.tgz '
                 'ConvertJob/Reports ConvertJob/Logs',
                 f'cd {out_dir}; tar zcvf logs-QCJob.tgz QCJob/logs',
@@ -228,17 +227,24 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
                 f'cd {out_dir}; tar zcvf prep-files.tgz '
                 'GenPrepFileJob/PrepFiles']
 
+        csv_fps = []
+        for root, dirs, files in walk(join(gpf_job.output_path, 'PrepFiles')):
+            for csv_file in files:
+                csv_fps.append(join(root, csv_file))
+
         for project, upload_dir in special_map:
             cmds.append(f'cd {out_dir}; tar zcvf reports-QCJob.tgz '
                         'QCJob/{project}/fastp_reports_dir')
             cmds.append(f'cd {out_dir}; mv '
                         'QCJob/{project}/filtered_sequences/* '
                         '{upload_dir}')
+            for csv_file in csv_fps:
+                if project in csv_file:
+                    cmds.append(f'cd {out_dir}; mv {csv_file} {upload_dir}')
+                    break
 
         cmds.append(f'cd {out_dir}; mv *.tgz final_results')
         cmds.append(f'cd {out_dir}; mv FastQCJob/multiqc final_results')
-        cmds.append(f'cd {out_dir}; mv GenPrepFileJob/PrepFiles/* '
-                    f'{upload_dir}')
 
         if skip_exec:
             cmds = []
@@ -255,17 +261,11 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
             ArtifactInfo('output', 'job-output-folder',
                          [(f'{final_results_path}/', 'directory')])
         ]
+    else:
+        success = False
+        msg = "Doesn't look like a valid uploaded file; please review."
 
-        qclient.update_job_step(job_id, "Main Pipeline Finished, processing "
-                                        "results")
-        return True, ainfo, None
+    qclient.update_job_step(job_id, "Main Pipeline Finished, processing "
+                                    "results")
 
-    except PipelineError as e:
-        # Pipeline() will identify directories and files that don't exist.
-        # Catch all PipelineErrors() here and handle them by returning
-        # success=False, None (ainfo), and a string representation of the
-        # error.
-        return False, None, str(e)
-
-    # sequence_processing_pipeline() should not arrive at this point.
-    raise PipelineError("sequence_processing_pipeline() did not complete.")
+    return success, ainfo, msg
