f0f7dd21982808a8c2b6566abf32f9dd7c4728b0
Charles Cowart <ccowart@ucsd.edu>
2021-11-02 11:22:24 -0700

    Changed directory to match GenPrepFileJob's expectations.

--- qp_klp/tests/test_klp.py
+++ qp_klp/tests/test_klp.py
@@ -280,9 +280,10 @@ class KLPTests(PluginTestCase):
             for line in self.multiqc_config_data:
                 f.write(f"{line}\n")
 
-        # create the Reports directory in run_dir
-        reports_dir = join(test_dir, 'Reports')
-        makedirs(reports_dir)
+        # create the Reports directory in the location GenPrepFileJob
+        # expects.
+        reports_dir = join(self.out_dir, 'ConvertJob', 'Reports')
+        makedirs(reports_dir, exist_ok=True)
 
         # create QCJobs output directory for use by GenPrepFileJob
         qcj_output_fp = join(self.out_dir, 'QCJob', 'Feist_11661')
