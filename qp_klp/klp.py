6aca8a58e081abc609d4b0660ce98e46a7eb543c
Charles Cowart <42684307+charles-cowart@users.noreply.github.com>
2022-02-25 11:55:21 -0800

    Move sif tarball to final_results and files to upload_dir (#13)
    * Move sif tarball to final_results and files to upload_dir
    
    * Move each individual uncompressed sif file to its proper upload dir
    
    * flake8
    
    * Update test_klp
    
    * add logging
    
    * Update move_fix
    
    Updated sif-file-related code to consider multiple projects, some w/out blanks.
    Force an error during unit-testing to confirm paths for test files.
    
    * Move fix to proper location.
    
    * Set walk() path to intended location
    
    * Test cmds.log parsing
    
    * Adjust cmds log generation
    
    Adjust cmds log so that a non-empty list of commands is generated, even when skip_exec is True and actual running of commands is bypassed. This allows for unit-testing of commands generation.
    
    * Compare cmds.log output w/expected results
    
    * Adjust test
    
    * Adjust test
    
    * Remove additional log output

--- qp_klp/klp.py
+++ qp_klp/klp.py
@@ -246,6 +246,12 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
                 csv_fps.append(join(root, csv_file))
 
         for project, upload_dir in special_map:
+            if sifs and [x for x in sifs if f'{project}_blanks.tsv' in x]:
+                # move uncompressed sifs to upload_dir.
+                # sif filenames are of the form: '{project}_blanks.tsv'
+                cmds.append(f'cd {out_dir}; mv {project}_blanks.tsv'
+                            f' {upload_dir}')
+
             cmds.append(f'cd {out_dir}; tar zcvf reports-QCJob.tgz '
                         f'QCJob/{project}/fastp_reports_dir')
 
@@ -263,19 +269,21 @@ def sequence_processing_pipeline(qclient, job_id, parameters, out_dir):
                     cmds.append(f'cd {out_dir}; mv {csv_file} {upload_dir}')
                     break
 
+        # copy all tgz files, including sample-files.tgz, to final_results.
         cmds.append(f'cd {out_dir}; mv *.tgz final_results')
         cmds.append(f'cd {out_dir}; mv FastQCJob/multiqc final_results')
 
-        if sifs:
-            cmds.append(f'cd {out_dir}; mv sample-files.tgz {upload_dir}')
-
-        if skip_exec:
-            cmds = []
-
+        # allow the writing of commands out to cmds.log, even if skip_exec
+        # is True. This allows for unit-testing of cmds generation.
         cmd_log_fp = join(out_dir, 'cmds.log')
         with open(cmd_log_fp, 'w') as cmd_log_f:
             for cmd in cmds:
                 cmd_log_f.write(f'{cmd}\n')
+
+        # if execution was skipped, reinitialze the cmds list to empty after
+        # writing to log and before actually executing commands.
+        if skip_exec:
+            cmds = []
 
         for cmd in cmds:
             p = Popen(cmd, universal_newlines=True, shell=True,
