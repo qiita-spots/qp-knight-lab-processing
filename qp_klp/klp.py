168e257af28c485ddacf218837a85d63cb79f9ad
Charles Cowart <ccowart@ucsd.edu>
2022-03-11 10:20:37 -0800

    flake8

--- qp_klp/klp.py
+++ qp_klp/klp.py
@@ -5,7 +5,6 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
-import json
 from functools import partial
 from inspect import stack
 from os import environ, walk
