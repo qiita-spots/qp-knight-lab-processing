97bb7933680a94663c8e655f0bd640439c155189
Charles Cowart <ccowart@ucsd.edu>
2022-03-15 12:47:25 -0700

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -51,8 +51,6 @@ setup(name='qp-klp',
                         'qiita_client @ https://github.com/'
                         'qiita-spots/qiita_client/archive/master.zip',
                         'sequence-processing-pipeline @ https://github.com/'
-                        'biocore/mg-scripts/archive/'
-                        'master.zip'
-                        ],
+                        'biocore/mg-scripts/archive/master.zip'],
       dependency_links=[],
       classifiers=classifiers)
