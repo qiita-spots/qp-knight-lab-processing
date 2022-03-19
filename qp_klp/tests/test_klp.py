4387bf6241002028dc0671992a7c9fd0dbb7476c
Charles Cowart <ccowart@ucsd.edu>
2022-03-19 14:28:39 -0700

    test

--- qp_klp/tests/test_klp.py
+++ qp_klp/tests/test_klp.py
@@ -368,8 +368,8 @@ class KLPTests(PluginTestCase):
                 'tp_reports_dir'),
                'cd PREFIX/support_files/test_data/uploads/11661',
                'cd OUT_DIR; mv *.tgz final_results',
-               'cd OUT_DIR; mv FastQCJob/multiqc final_results',
-               'cd OUT_DIR; mv touched_studies.html final_results']
+               'cd OUT_DIR; mv FastQCJob/multiqc final_results']
+        # 'cd OUT_DIR; mv touched_studies.html final_results']
 
         cmdslog_fp = join(self.out_dir, 'cmds.log')
         with open(cmdslog_fp, 'r') as f:
