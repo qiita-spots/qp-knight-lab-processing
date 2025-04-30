55bf4207fad7534ac3cbd17f0540a5e56ba864e3
Antonio Gonzalez <antgonza@gmail.com>
2025-04-30 13:51:35 -0600

    fix tests

--- src/qp_klp/Workflows.py
+++ src/qp_klp/Workflows.py
@@ -504,8 +504,8 @@ class Workflow():
             results_sn = self._process_sample_names(p_name, qiita_id,
                                                     samples)
 
-            msgs.append("Number of values in sheet that aren't sample-names in"
-                        " Qiita: %s" % len(results_sn[0]))
+            msgs.append('Number of sample-names not in Qiita: '
+                        f'{len(results_sn[0])}; {results_sn[0][:3]}')
 
             use_tids = False
 
@@ -517,8 +517,8 @@ class Workflow():
                 # check for possible match w/tube-ids, if defined in project.
                 results_tid = self._process_tube_ids(qiita_id, samples)
                 if results_tid:
-                    msgs.append("Number of values in sheet that aren't "
-                                "tube-ids in Qiita: %s" % len(results_tid[0]))
+                    msgs.append('Number of sample-names not in Qiita: '
+                                f'{len(results_tid[0])}; {results_tid[0][:3]}')
 
                     if len(results_tid[0]) == 0:
                         # all values were matched to tube-ids.
