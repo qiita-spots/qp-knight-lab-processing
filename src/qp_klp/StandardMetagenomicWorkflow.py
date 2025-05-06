b8e847a8a46e59539398a5cc853d6b55081606b6
Antonio Gonzalez <antgonza@gmail.com>
2025-05-06 11:15:06 -0600

    adding main changes

--- src/qp_klp/StandardMetagenomicWorkflow.py
+++ src/qp_klp/StandardMetagenomicWorkflow.py
@@ -1,9 +1,18 @@
+from functools import partial
+import sample_sheet
+import pandas as pd
+from os.path import basename, join
+from os import symlink, makedirs
+from datetime import datetime
+from metapool import MetagenomicSampleSheetv90
+from pathlib import Path
+
 from .Protocol import Illumina
 from sequence_processing_pipeline.Pipeline import Pipeline
 from .Assays import Metagenomic
 from .Assays import ASSAY_NAME_METAGENOMIC
 from .FailedSamplesRecord import FailedSamplesRecord
-from .Workflows import Workflow
+from .Workflows import Workflow, WorkflowError
 
 
 class StandardMetagenomicWorkflow(Workflow, Metagenomic, Illumina):
@@ -49,3 +58,130 @@ class StandardMetagenomicWorkflow(Workflow, Metagenomic, Illumina):
                                  "type bool")
 
             self.update = kwargs['update_qiita']
+
+
+class PrepNuQCWorkflow(StandardMetagenomicWorkflow):
+    def __init__(self, **kwargs):
+        qclient = kwargs['qclient']
+        job_id = kwargs['job_id']
+        parameters = kwargs['parameters']
+        out_dir = kwargs['out_dir']
+        config_fp = kwargs['config_fp']
+        status_line = kwargs['status_line']
+
+        out_path = partial(join, out_dir)
+        self.final_results_path = out_path('final_results')
+        makedirs(self.final_results_path, exist_ok=True)
+
+        pid = parameters.pop('prep_id')
+
+        prep_info = qclient.get(f'/qiita_db/prep_template/{pid}/')
+        dt = prep_info['data_type']
+        sid = prep_info['study']
+        if dt not in {'Metagenomic', 'Metatranscriptomic'}:
+            raise WorkflowError(f'Prep {pid} has a not valid data type: {dt}')
+        aid = prep_info['artifact']
+        if not str(aid).isnumeric():
+            raise WorkflowError(f'Prep {pid} has a not valid artifact: {aid}')
+
+        files, pt = qclient.artifact_and_preparation_files(aid)
+        html_summary = qclient.get_artifact_html_summary(aid)
+        if html_summary is None:
+            raise WorkflowError(f'Artifact {aid} doesnot have a summary, '
+                                'please generate one.')
+        df_summary = pd.read_html(html_summary)[0]
+        pt.set_index('sample_name', inplace=True)
+
+        project_name = f'qiita-{pid}-{aid}_{sid}'
+
+        sheet = MetagenomicSampleSheetv90()
+        sheet.Header['IEMFileVersion'] = '4'
+        sheet.Header['Date'] = datetime.today().strftime('%m/%d/%y')
+        sheet.Header['Workflow'] = 'GenerateFASTQ'
+        sheet.Header['Application'] = 'FASTQ Only'
+        sheet.Header['Assay'] = prep_info['data_type']
+        sheet.Header['Description'] = f'prep_NuQCJob - {pid}'
+        sheet.Header['Chemistry'] = 'Default'
+        sheet.Header['SheetType'] = 'standard_metag'
+        sheet.Header['SheetVersion'] = '90'
+        sheet.Header['Investigator Name'] = 'Qiita'
+        sheet.Header['Experiment Name'] = project_name
+
+        sheet.Bioinformatics = pd.DataFrame(
+            columns=['Sample_Project', 'ForwardAdapter', 'ReverseAdapter',
+                     'library_construction_protocol',
+                     'experiment_design_description',
+                     'PolyGTrimming', 'HumanFiltering', 'QiitaID'],
+            data=[[project_name, 'NA', 'NA', 'NA', 'NA',
+                   'FALSE', 'TRUE', sid]])
+
+        df_summary = df_summary[df_summary.file_type == 'raw_forward_seqs']
+        data = []
+        for k, vals in pt.iterrows():
+            k = k.split('.', 1)[-1]
+            rp = vals['run_prefix']
+            sample = {
+                'Sample_Name': k,
+                'Sample_ID': k.replace('.', '_'),
+                'Sample_Plate': '',
+                'well_id_384': '',
+                'I7_Index_ID': '',
+                'index': vals['index'],
+                'I5_Index_ID': '',
+                'index2': vals['index2'],
+                'Sample_Project': project_name,
+                'Well_description': '',
+                'Sample_Well': '',
+                'Lane': '1'}
+            sheet.add_sample(sample_sheet.Sample(sample))
+            _d = df_summary[
+                df_summary.filename.str.startswith(rp)]
+            if _d.shape[0] != 1:
+                ValueError(f'The run_prefix {rp} from {k} has {_d.shape[0]} '
+                           'matches with files')
+            data.append({
+                'Lane': '1', 'SampleID': rp, 'Sample_Project': project_name,
+                'Index': vals['index'], '# Reads': _d.reads.values[0]})
+
+        sheet.Contact = pd.DataFrame(
+            columns=['Email', 'Sample_Project'],
+            data=[['qiita.help@gmail.com', project_name]])
+
+        new_sample_sheet = out_path('sample-sheet.csv')
+        with open(new_sample_sheet, 'w') as f:
+            sheet.write(f, 1)
+
+        # now that we have a sample_sheet we can fake the
+        # ConvertJob folder so we are ready for the restart
+        convert_path = out_path('ConvertJob')
+        project_folder = out_path('ConvertJob', project_name)
+        makedirs(project_folder, exist_ok=True)
+        # creating Demultiplex_Stats.csv
+        reports_folder = out_path('ConvertJob', 'Reports')
+        makedirs(reports_folder, exist_ok=True)
+        pd.DataFrame(data).set_index('SampleID').to_csv(
+            f'{reports_folder}/Demultiplex_Stats.csv')
+
+        for fs in files.values():
+            for f in fs:
+                bn = basename(f['filepath']).replace(
+                    '.trimmed.fastq.gz', '.fastq.gz')
+                symlink(f['filepath'], f'{project_folder}/{bn}')
+
+        # create job_completed file to skip this step
+        Path(f'{convert_path}/job_completed').touch()
+
+        kwargs = {'qclient': qclient,
+                  'uif_path': new_sample_sheet,
+                  'lane_number': "1",
+                  'config_fp': config_fp,
+                  'run_identifier': '250225_LH00444_0301_B22N7T2LT4',
+                  'output_dir': out_dir,
+                  'job_id': job_id,
+                  'status_update_callback': status_line.update_job_status,
+                  # set 'update_qiita' to False to avoid updating Qiita DB
+                  # and copying files into uploads dir. Useful for testing.
+                  'update_qiita': True,
+                  'is_restart': True}
+
+        super().__init__(**kwargs)
---
+++ tests/test_PrepNuQCWorkflow.py
@@ -0,0 +1,102 @@
+from unittest import main
+from os.path import join, exists, isdir
+from os import remove
+from shutil import rmtree, copyfile
+from tempfile import mkdtemp
+from qp_klp import plugin, __version__ as version_qp_klp
+from qp_klp.klp import PrepNuQCJob
+
+from qiita_client.testing import PluginTestCase
+from json import dumps
+
+
+class TestPrepNuQCWorkflow(PluginTestCase):
+    def setUp(self):
+        plugin("https://localhost:21174", 'register', 'ignored')
+
+        out_dir = mkdtemp()
+        self.maxDiff = None
+        self.out_dir = out_dir
+        self._clean_up_files = []
+        self._clean_up_files.append(out_dir)
+
+    def tearDown(self):
+        for fp in self._clean_up_files:
+            if exists(fp):
+                if isdir(fp):
+                    rmtree(fp)
+                else:
+                    remove(fp)
+
+    def _setup_test(self):
+        prep_info_dict = {
+            'SKB8.640193': {'run_prefix': 'S22205_S104', 'index': 'GTCACTGT',
+                            'index2': 'GCAAGATC'},
+            'SKD8.640184': {'run_prefix': 'S22282_S102', 'index': 'GCCATAAC',
+                            'index2': 'AGTCTCAC'}}
+        data = {'prep_info': dumps(prep_info_dict),
+                # magic #1 = testing study
+                'study': 1,
+                'data_type': 'Metagenomic'}
+        pid = self.qclient.post('/apitest/prep_template/', data=data)['prep']
+
+        # inserting artifacts
+        in_dir = mkdtemp()
+        self._clean_up_files.append(in_dir)
+
+        source_gz = 'tests/data/dummy.fastq.gz'
+        fps = [
+            join(in_dir, 'S22205_S104_L001_R1_001.fastq.gz'),
+            join(in_dir, 'S22205_S104_L001_R2_001.fastq.gz'),
+            join(in_dir, 'S22282_S102_L001_R1_001.fastq.gz'),
+            join(in_dir, 'S22282_S102_L001_R2_001.fastq.gz')
+        ]
+        for fp in fps:
+            copyfile(source_gz, fp)
+        fp_summary = join(in_dir, 'summary.html')
+        copyfile('tests/data/summary.html', fp_summary)
+
+        data = {
+            'filepaths': dumps([
+                (fps[0], 'raw_forward_seqs'),
+                (fps[1], 'raw_reverse_seqs'),
+                (fps[2], 'raw_forward_seqs'),
+                (fps[3], 'raw_reverse_seqs'),
+                (fp_summary, 'html_summary')]),
+            'type': "per_sample_FASTQ",
+            'name': "Test Woltka artifact",
+            'prep': pid}
+        self.qclient.post('/apitest/artifact/', data=data)['artifact']
+
+        data = {'user': 'demo@microbio.me',
+                'command': dumps(['qp-klp', version_qp_klp,
+                                  'Human Filter & QC existing Prep']),
+                'status': 'running',
+                'parameters': dumps({'prep_id': pid})}
+        job_id = self.qclient.post(
+            '/apitest/processing_job/', data=data)['job']
+
+        return pid, job_id
+
+    def test_prep_nuqcjob(self):
+        out_dir = mkdtemp()
+        self._clean_up_files.append(out_dir)
+
+        # test failure
+        jid = 'bcc7ebcd-39c1-43e4-af2d-822e3589f14d'
+        success, ainfo, msg = PrepNuQCJob(
+            self.qclient, jid, {'prep_id': '1'}, out_dir)
+        self.assertEqual(msg, "Prep 1 has a not valid data type: 18S")
+        self.assertFalse(success)
+
+        # test success; note that success at this point is that
+        # the job fails when submitting via sbatch
+        pid, job_id = self._setup_test()
+        success, ainfo, msg = PrepNuQCJob(
+            self.qclient, job_id, {'prep_id': pid}, out_dir)
+        self.assertRegex(msg, r'(?s)sbatch:(?s)found(?s)')
+        self.assertFalse(success)
+
+
+if __name__ == '__main__':
+    main()
