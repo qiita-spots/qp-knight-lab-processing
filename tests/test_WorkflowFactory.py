78ae47dd5ca0a8038d89f6f3c887d0b4c2bde87b
Antonio Gonzalez <antgonza@gmail.com>
2025-07-17 14:52:34 -0600

    changing tests

--- tests/test_WorkflowFactory.py
+++ tests/test_WorkflowFactory.py
@@ -222,8 +222,14 @@ class WorkflowFactoryTests(PluginTestCase):
         # and the system is trying to create the preps.
         # Note: Qiita job_id's are UUID in the database and this tests
         # uses 78901 as the job_id so the db complains about the format
-        with self.assertRaisesRegex(RuntimeError, 'invalid input '
-                                    'syntax for type uuid: "78901"'):
+        # Note 07.17.25: for whatever reason the reponse for this test
+        #                changed and is no longer 'invalid input '
+        #                'syntax for type uuid: "78901"; leaving this
+        #                note for future reference
+
+        with self.assertRaisesRegex(
+                RuntimeError, "Request 'post https://localhost:21174/"
+                "qiita_db/prep_template/' did not succeed"):
             wf.execute_pipeline()
 
     def test_metatranscriptomic_workflow_creation(self):
